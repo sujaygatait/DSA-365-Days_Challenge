5b2cd5e8e19ff4f8b7facbb7a92778e7b11efa6b
Sujay Gatait <155906326+sujaygatait@users.noreply.github.com>
2026-07-25 12:28:47 +0530

    Update 1283.java

--- LeetCode_Solves/1283.java
+++ LeetCode_Solves/1283.java
@@ -1,3 +1,4 @@
+// Method-1
 class Solution {
     public int smallestDivisor(int[] nums, int threshold) {
         int n = nums.length;
@@ -18,6 +19,42 @@ class Solution {
             }
 
             if(sum <= threshold) {
+                divisor = mid;
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+
+        return divisor;
+    }
+}
+
+
+// Method-2
+class Solution {
+    static boolean isLess(int[] nums, int mid, int threshold) {
+        int sum = 0;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] % mid == 0) sum += nums[i]/mid;
+            else sum += nums[i]/mid + 1;
+        }
+
+        if(sum <= threshold) return true;
+        else return false;
+    }
+    public int smallestDivisor(int[] nums, int threshold) {
+        int n = nums.length;
+        int max = nums[0];
+        for(int i=0; i<nums.length; i++) {
+            max = Math.max(max, nums[i]);
+        }
+
+        int divisor = -1;
+        int low=1, high=max;
+        while(low <= high) {
+            int mid = low + (high-low)/2;
+
+            if(isLess(nums, mid, threshold)) {
                 divisor = mid;
                 high = mid - 1;
             }
