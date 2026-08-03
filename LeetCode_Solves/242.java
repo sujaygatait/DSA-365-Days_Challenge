ca439981385cdcac3bc4ab8f225a2bd74ca2a765
Sujay Gatait <155906326+sujaygatait@users.noreply.github.com>
2026-08-03 22:18:13 +0530

    Update 242.java

--- LeetCode_Solves/242.java
+++ LeetCode_Solves/242.java
@@ -13,3 +13,35 @@ class Solution {
         return false;
     }
 }
+
+
+// Method - 2
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> hm = new HashMap<>();
+
+        for(int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+        }
+
+        for(int i=0; i<t.length(); i++) {
+            char ch = t.charAt(i);
+            if(hm.get(ch) != null) {
+                if(hm.get(ch) == 1) {
+                    hm.remove(ch);
+                }
+                else {
+                    hm.put(ch, hm.get(ch) - 1);
+                }
+            }
+            else {
+                return false;
+            }
+        }
+
+        return hm.isEmpty();
+    }
+}
